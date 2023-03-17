e0ffd8b5208b6b18af69887f7ceac339528de46f
Ravi Mone <ravimone@gmail.com>
2023-03-17 09:59:29 +0530

    code refactor

--- src/Components/SortBy/SortBy.js
+++ src/Components/SortBy/SortBy.js
@@ -1,10 +1,10 @@
 import React, { useState, useContext } from "react";
 import { ProductContext } from "../../Services/Product/ProductContext";
-import {FETCH_PRODUCTS} from "../../Services/Product/action-types";
+import { FETCH_PRODUCTS } from "../../Services/Product/action-types";
 
 const SortBy = () => {
   const [sortValue, setSortValue] = useState();
-  const {products,productDispatch} = useContext(ProductContext);
+  const { products, productDispatch } = useContext(ProductContext);
   const compare = {
     lowestprice: (a, b) => {
       if (parseInt(a.productPrice) < parseInt(b.productPrice)) return -1;
@@ -15,22 +15,29 @@ const SortBy = () => {
       if (parseInt(a.productPrice) > parseInt(b.productPrice)) return -1;
       if (parseInt(a.productPrice) < parseInt(b.productPrice)) return 1;
       return 0;
-    }
+    },
   };
 
   const handleChange = (event) => {
-    products.items.sort(compare[event.target.value]);
-    setSortValue(event.target.value);
+    const { value } = event.target;
+    products.items.sort(compare[value]);
+    setSortValue(value);
     productDispatch({
-        type: FETCH_PRODUCTS,
-        payload: products.items
-      });
+      type: FETCH_PRODUCTS,
+      payload: products.items,
+    });
   };
   return (
     <div className="mr-10 mb-5">
       <label>Sort by:</label>
-      <select value={sortValue} onChange={(ev) => handleChange(ev)} className="border ml-2 p-2">
-        <option value='1' disabled selected>Select</option>
+      <select
+        value={sortValue}
+        onChange={(ev) => handleChange(ev)}
+        className="border ml-2 p-2"
+      >
+        <option value="1" disabled selected>
+          Select
+        </option>
         <option value="lowestprice">Lowest to Highest</option>
         <option value="highestprice">Highest to Lowest</option>
       </select>
